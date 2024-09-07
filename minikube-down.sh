b04d5b9ed10c1700dc240a95043ab304e3080f55
Lara <larasr@estudante.ufscar.br>
2024-09-07 15:13:39 -0300

    all scripts done (just testing now)

---
+++ minikube-down.sh
@@ -0,0 +1,24 @@
+# Removendo a configuração do docker do backend 
+cd K8s/site
+
+kubectl delete -f deployment.yaml; kubectl delete -f service.yaml;
+
+cd ../..
+
+# Removendo a configuração do docker do nginx
+cd K8s/book-covers
+
+kubectl delete -f deployment.yaml; kubectl delete -f service.yaml;
+
+cd ../..
+
+# Removendo a configuração do docker do banco de dados
+cd K8s/database
+
+kubectl delete -f deployment.yaml; kubectl delete -f service.yaml;
+
+kubectl delete -f configmap.yaml; kubectl delete -f secret.yaml;
+
+kubetctl delete -f pv.yaml; kubectl delete -f pvc.yaml;
+
+cd ../..
---
+++ minikube-up.sh
@@ -0,0 +1,66 @@
+# Gerando a imagem do docker do banco de dados
+cd docker-img/database
+
+cp ../../db/MySQL/create.sql .
+
+docker build . -t db
+
+minikube image load db
+
+# Gerando a imagem do docker do nginx referente a capa dos livros
+cd ../book-covers
+
+cp -r ../../book-covers .
+
+docker build . -t book-covers
+
+minikube image load book-covers
+
+# Gerando a imagem do docker do backend 
+cd ../site
+
+cp ../../target/Livraria.war .
+
+docker build . -t web
+
+minikube image load web 
+
+cd ..
+
+# Inicializando o minikube                 
+minikube start
+
+# Inicializando os complementos necessários             
+minikube addons enable ingress
+
+minikube addons enable dashboard
+
+minikube addons enable metrics-server
+
+################# Configurando o K8s
+# 1) Banco de dados (db)
+cd K8s/database
+
+kubetctl create -f pv.yaml; kubectl create -f pvc.yaml;
+
+kubectl apply -f configmap.yaml; kubectl apply -f secret.yaml;
+
+kubectl apply -f deployment.yaml; kubectl apply -f service.yaml;
+
+cd ../..
+
+# 2) Nginx - capa dos livros (book-covers)
+cd K8s/book-covers
+
+kubectl apply -f deployment.yaml; kubectl apply -f service.yaml;
+
+cd../..
+
+# 3) Website - backend (web)
+cd K8s/site
+
+kubectl wait --for=jsonpath='{.status.phase}'=Running $(kubectl get pods -o=name)
+
+kubectl apply -f deployment.yaml; kubectl apply -f service.yaml;
+
+cd../..
